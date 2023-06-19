6a0de372e93d913595c4c1f222d73c884d6f4d36
Furkan Keskin <ksknmhmt3131@gmail.com>
2023-06-19 14:15:47 +0300

    Initial Commit

---
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by Furkan on 19.06.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+
+}
---
+++ NetflixClone/Controllers/UpCommingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpCommingViewController.swift
+//  NetflixClone
+//
+//  Created by Furkan on 19.06.2023.
+//
+
+import UIKit
+
+class UpCommingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
